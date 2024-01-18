3534e915aca9c67fb1144890cd8a3d39aa3cbcd9
Edilson Santos <35012659+Edilsonss123@users.noreply.github.com>
2024-01-18 20:47:23 -0300

    Update db.go

--- app/database/db.go
+++ app/database/db.go
@@ -16,8 +16,11 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	bdHost := os.Getenv("BD_HOST")
-	stringDeConexao := fmt.Sprintf("host=%s user=root password=root dbname=root port=5432 sslmode=disable", bdHost)
+	dbHost := os.Getenv("DB_HOST")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	stringDeConexao := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", dbHost, dbPassword, dbName, dbPort)
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
